5dd1d76abb96e6c6f8627b954d10e2befafa5b2c
JaseFreemanBirdsnest <jason.freeman@birdsnest.com.au>
2023-01-31 17:06:04 +1100

    Improved on Game functionality

--- game.js
+++ game.js
@@ -1,25 +1,26 @@
 
-let choices = ["Rock", "Paper", "Scissors"]; // The array of choices.
+let choices = ["Rock", "Paper", "Scissors"]; 
 const btn = document.querySelectorAll('button')
 
 const results = [];
 
-//let getComputerChoice = Math.floor(Math.random() * choices.length); // Selects a random array position
+
 
 function getComputerChoice(choices){
 
-    return choices.at(Math.floor(Math.random() * choices.length)).toLowerCase(); // Gets the string associated with the index position selected above
+    return choices.at(Math.floor(Math.random() * choices.length)).toLowerCase();
 }
-
-//2. Get the players throw by prompting for either Rock, Paper, Scissors.
-    // This will need to remove case
 
 
 function getPlayerChoice(choices){
     
     btn.forEach((button) => {
         button.addEventListener('click', () => {
-            return button.id.toLowerCase();
+            if (button.id !== "Rock" || button.id !== "Paper" || button.id !== "Scissors"){
+                return
+            };            
+                return button.id.toLowerCase();
+            
         });
     });
 
@@ -27,70 +28,50 @@ function getPlayerChoice(choices){
 
 }
 
-
-
-
-//3. Play a round using computer choice and player choice
 function playRound(playerThrow, computerThrow){
 
-
-
-//get computer choice and set for this round
 var computerThrow = getComputerChoice(choices);
-//get players choice and set for this round
+
 var playerThrow = getPlayerChoice(choices);
 
-
-
-
-    //  win / lose condition alerts that only needs to be written once
     let winAlert = ("You Win! You threw " +  playerThrow + " which beats the computers throw of " + computerThrow);
     let loseAlert = ("You Lose! The computer threw " + computerThrow + " which beats your throw of " + playerThrow);
 
-    //Tie Condition i.e Computer and Player both choose the same
     if (computerThrow == playerThrow){
-        results.push("Tie"); //add result to results array.
-        return alert("Its a Tie"); //alert the player of the results of this round.
+        results.push("Tie"); 
+        return alert("Its a Tie"); 
     }
-    //Rock breaks Scissors
-    //if getComputerChoice == "rock" && getPlayerChoice == "scissors"
+
 
     else if(computerThrow == "rock" && playerThrow == "scissors"){
         
         results.push("Computer");
         return alert(loseAlert);
     }
-    //if getPlayerChoice == "rock"&& getComputerChoice == "scissors"
         
     else if( playerThrow == "rock" && computerThrow == "scissors"){
         
         results.push("Player");
         return alert(winAlert);
     }
-    //Scissors cut Paper
-        //if getComputerChoice == "scissors" && getPlayerChoice == "paper"
             
     else if(computerThrow == "scissors" && playerThrow == "paper"){
         
         results.push("Computer");
         return alert(loseAlert);
     }
-        //if getPlayerChoice == "scissors"&& getComputerChoice == "paper"
             
     else if( playerThrow == "scissors" && computerThrow == "paper"){
         
         results.push("Player");
         return alert(winAlert);
     }
-    //Paper wraps Rock
-        //if getComputerChoice == "paper" && getPlayerChoice == "rock"
             
     else if(computerThrow == "paper" && playerThrow == "rock"){
         
         results.push("Computer");
         return alert(loseAlert);
     }
-        //if getPlayerChoice == "paper"&& getComputerChoice == "rock"
             
     else if( playerThrow == "paper" && computerThrow == "rock"){
         
@@ -99,12 +80,10 @@ var playerThrow = getPlayerChoice(choices);
     }
 }
 
-function game(playerThrow,computerThrow){      
-//create computer choice variable
-var computerThrow;
-//create players choice variable
-var playerThrow; 
+function game(playerThrow,computerThrow){    
 
+var computerThrow;
+var playerThrow; 
 
     for(let rounds = 0; rounds < 5; rounds++){
         playRound(playerThrow,computerThrow);
@@ -137,9 +116,9 @@ var playerThrow;
 }
 
 
-console.log(choices);
-console.log(getComputerChoice(choices));
-console.log(getPlayerChoice(choices));
+// console.log(choices);
+// console.log(getComputerChoice(choices));
+// console.log(getPlayerChoice(choices));
 
 
-game();
+btn.addEventListener("click", game());
